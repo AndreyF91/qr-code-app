9754b6269052f8dd9684f6bfaa29447a6cf12b53
AndreyF <getsuga@tut.by>
2021-12-21 17:27:38 +0300

    Add ChangeMode btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import Generator from './components/Generator';
+import { useState } from "react";
+import "./App.css";
+import Generator from "./components/Generator";
+import Reader from "./components/Reader";
 
 function App() {
+  const [mode, setMode] = useState(true);
+
   return (
     <div className="App">
-      <Generator />
+      <button onClick={() => setMode(!mode)}>Нажми</button>
+      {!mode ? <Generator /> : <Reader />}
     </div>
   );
 }
