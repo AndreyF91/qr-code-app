9e47567bf8a78a05b8de5a8f05b603a9abc6d694
AndreyF <getsuga@tut.by>
2021-12-22 19:45:08 +0300

    Add Error-message field

--- src/components/Reader.jsx
+++ src/components/Reader.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
 import QrReader from "react-qr-reader";
-import './Reader.scss';
+import "./Reader.scss";
 
 const Reader = () => {
   const [result, setResult] = useState("Результата нет");
   const [error, setError] = useState("");
 
-  console.log(error);
-
   return (
     <div className="reader__container">
       <QrReader
+      className="reader__camera"
         delay={300}
         onError={setError}
         onScan={setResult}
-        style={{ width: "100%", margin: '30px auto', padding:'10px' }}
+        style={{ width: "100%", margin: "30px auto", padding: "10px" }}
       />
-      <p className="reader__result"  onClick={() => {navigator.clipboard.writeText(result)}}>{result}</p>
+      {error ? <p className="reader__error">{error.message}</p> : null}
+      <p
+        className="reader__result"
+        onClick={() => {
+          navigator.clipboard.writeText(result);
+        }}
+      >
+        {result}
+      </p>
     </div>
   );
 };
